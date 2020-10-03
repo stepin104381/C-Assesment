50aab4ac187ac70169177fd857eed460bafee152
nisargahs <57631871+stepin104381@users.noreply.github.com>
2020-10-03 03:07:48 -0700

    Add files via upload

---
+++ SampleMvcApp/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace SampleMvcApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public int EmployeeSalary { get; set; }
+
+        public override string ToString()
+        {
+            return $"<p> The Name:{EmployeeName}</p><p>The Address:{EmployeeAddress}</p><p>Employee Salary:{EmployeeSalary:C}";
+        }
+
+
+    }
+}
